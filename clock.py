b83f2de2e5544351608506ab591398597d467bdf
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-19 09:31:11 +0200

    implement Digit Sprite

--- clock.py
+++ clock.py
@@ -1,7 +1,21 @@
 import pygame
+import pygame.freetype
+
+class Digit(pygame.sprite.Sprite):
+    def __init__(self, digit, width, height, pos_x, pos_y, color):
+        super().__init__()
+        self.digit = digit
+        font = pygame.freetype.Font(None, 100)
+        self.image = pygame.Surface((width, height))
+        font.render_to(self.image, (0, 0), str(self.digit), color)
+        self.font = pygame.freetype.Font(None)
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+
 
 def main():
     pygame.init()
+    pygame.freetype.init()
 
     screen_width = 1280
     screen_height = 720
@@ -14,11 +28,19 @@ def main():
     run = True
     game_speed = 10
     while run:
+        window.fill((0,0,0))
         clock.tick(game_speed)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        digit= Digit(1, 50, 100, 100, 100, (255, 255, 255))
+        digit_group = pygame.sprite.Group()
+        digit_group.add(digit)
+        digit_group.draw(window)
+        
+        pygame.display.update()
 
 
 if __name__ == '__main__':
