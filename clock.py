31f248407e8e9cfcf965ae74044767ca3355b490
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-20 20:21:19 +0200

    add second lines

--- clock.py
+++ clock.py
@@ -33,18 +33,16 @@ class Circle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (pos['x'], pos['y'])
 
+
 def add_circle(center, radius, window):
     circle_group = pygame.sprite.Group()
     circle_white = Circle(radius, (255, 255, 255), center)
     circle_group.add(circle_white)
     circle_group.draw(window)
 
-def add_numbers(center, radius, window):
-    size = 80
 
+def add_numbers(center_vector, radius_vector, size, window):
     angle = 360/12
-    center_vector = pygame.math.Vector2(center['x'], center['y'])
-    radius_vector = pygame.math.Vector2(0, -radius - size/1.5)
     number_vectors = []
     
     radius_vectors = [radius_vector for vector in range(0, 12)]
@@ -62,6 +60,17 @@ def add_numbers(center, radius, window):
     numbers_group.draw(window)
 
 
+def add_second_lines(center_vector, radius, window):
+    second_line_begin_vector = pygame.math.Vector2(0, -230)
+    second_line_end_vector = pygame.math.Vector2(0, -radius+5)
+    angle_offset = 360/60
+
+    for angle in range(0, 60):
+        second_line_begin_vector2 = second_line_begin_vector.rotate(angle*angle_offset)
+        second_line_end_vector2 = second_line_end_vector.rotate(angle*angle_offset)
+        pygame.draw.line(window, (255, 255, 255), center_vector + second_line_begin_vector2, center_vector + second_line_end_vector2, width=5)
+
+
 def main():
     pygame.init()
     pygame.freetype.init()
@@ -72,16 +81,19 @@ def main():
     window = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Clock")
 
-    # variables
+    # constants
     center = {'x' : screen_width/2, 'y': screen_height/2}
     radius = screen_width/2 - 100
+    size = 80
+    center_vector = pygame.math.Vector2(center['x'], center['y'])
+    radius_vector = pygame.math.Vector2(0, -radius - size/1.5)
 
     clock = pygame.time.Clock()
 
     run = True
     game_speed = 120
     while run:
-        window.fill((0,0,0))
+        window.fill((0, 0, 0))
         clock.tick(game_speed)
 
         for event in pygame.event.get():
@@ -89,7 +101,8 @@ def main():
                 run = False
 
         add_circle(center, radius, window)
-        add_numbers(center, radius, window)
+        add_numbers(center_vector, radius_vector, size, window)
+        add_second_lines(center_vector, radius, window)
 
         pygame.display.update()
 
