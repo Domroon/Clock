7505f480342d472a659c3efc0720da3316c9c63b
Dominik Haeusser <haeusser-dominik@web.de>
2021-05-20 12:56:54 +0200

    improve Digit Class

--- clock.py
+++ clock.py
@@ -1,16 +1,29 @@
 import pygame
+from pygame import image
 import pygame.freetype
 
 class Digit(pygame.sprite.Sprite):
     def __init__(self, digit, size, color, pos_x, pos_y):
         super().__init__()
-        self.digit = digit
+        self.size = size
         font = pygame.freetype.Font(None, size)
-        self.image = pygame.Surface((size/2, size - 20))
-        font.render_to(self.image, (0, 0), str(self.digit), color)
+        self.image = digit
+        font.render_to(self.image, (0, 0), digit, color)
         self.font = pygame.freetype.Font(None)
         self.rect = self.image.get_rect()
         self.rect.center = (pos_x, pos_y)
+
+    @property
+    def image(self):
+        return self._image
+
+    @image.setter
+    def image(self, digit):
+        if len(digit) > 1:
+            self._image = pygame.Surface((self.size + 5, self.size - 20))
+        else:
+            self._image = pygame.Surface((self.size/2, self.size - 20))
+
 
 class Number:
     def __init__(self, number, size, color, pos_x, pos_y):
@@ -59,34 +72,37 @@ def main():
         circle_group.add(circle_white)
         circle_group.draw(window)
 
-        size = 100
+        size = 80
 
         # 360/12 are the angles for the numbers
         angle = 360/12
         center_vector = pygame.math.Vector2(center['x'], center['y'])
-        radius_vector = pygame.math.Vector2(0, -radius - size/2)
+        radius_vector = pygame.math.Vector2(0, -radius - size/1.5)
         number_vectors = []
     
         radius_vectors = [radius_vector for vector in range(1, 12)]
         for vector in radius_vectors:
             vector = vector.rotate(int(angle))
             number_vectors.append(center_vector + vector)
-            angle += angle
+            angle += 360/12
 
         # write alle 12 vectors in a new list and write an every place center_vector + radius_vector! then you have all correct vectors!
-        digit_1 = Digit(1, size, (255, 255, 255), number_vectors[0].x, number_vectors[0].y)
-        digit_2 = Digit(2, size, (255, 255, 255), number_vectors[1].x, number_vectors[1].y)
-        digit_6 = Digit(6, size, (255, 255, 255), center['x'], center['y'] + radius + size/2)
-        digit_9 = Digit(9, size, (255, 255, 255), center['x'] - radius - size/3, center['y'])
+        digit_1 = Digit("1", size, (255, 255, 255), number_vectors[0].x, number_vectors[0].y)
+        digit_2 = Digit("2", size, (255, 255, 255), number_vectors[1].x, number_vectors[1].y)
+        digit_3 = Digit("3", size, (255, 255, 255), number_vectors[2].x, number_vectors[2].y)
+        digit_4 = Digit("4", size, (255, 255, 255), number_vectors[3].x, number_vectors[3].y)
+        digit_5 = Digit("5", size, (255, 255, 255), number_vectors[4].x, number_vectors[4].y)
+        digit_6 = Digit("6", size, (255, 255, 255), number_vectors[5].x, number_vectors[5].y)
+        digit_7 = Digit("7", size, (255, 255, 255), number_vectors[6].x, number_vectors[6].y)
         digit_group = pygame.sprite.Group()
-        digit_group.add(digit_2, digit_6, digit_9, digit_1)
+        digit_group.add(digit_1, digit_2, digit_3, digit_4, digit_5,digit_6,digit_7)
         digit_group.draw(window)
 
-        number_group = pygame.sprite.Group()
-        vector_12 = center_vector + radius_vector
-        number = Number("12", size, (255, 255, 255), vector_12.x, vector_12.y)
-        number_group.add(number.digit_front, number.digit_back)
-        number_group.draw(window)
+        #number_group = pygame.sprite.Group()
+        #vector_12 = center_vector + radius_vector
+        #number = Number("12", size, (255, 255, 255), vector_12.x, vector_12.y)
+        #number_group.add(number.digit_front, number.digit_back)
+        #number_group.draw(window)
         
         
         pygame.display.update()
